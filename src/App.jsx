3b4868d16a41607752b5137f23024e31e09bcff7
Ismayil Ismayilov <Psixo2015@mail.ru>
2024-03-31 21:54:19 +0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+   return (
+      <h1 className="text-1xl font-bold underline">
+         Hello world!
+         <Home />
+      </h1>
+   )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+   return (
+      <div className="text-green-400 text-3xl">Home</div>
+   )
+}
+
+export default Home
