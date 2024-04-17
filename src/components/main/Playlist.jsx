23547b636b3d23876ba991a6e8d2a8214d128826
Ismayil Ismayilov <Psixo2015@mail.ru>
2024-04-17 19:25:44 +0400

    add context menu state

--- src/components/main/Playlist.jsx
+++ src/components/main/Playlist.jsx
@@ -4,6 +4,7 @@ import PlaylistDescr from "./PlaylistDescr"
 import PlaylistImg from "./PlaylistImg"
 import PlaylistTitle from "./PlaylistTitle"
 import PlaylistContextMenu from "./PlaylistContextMenu"
+import { useState } from "react"
 
 const menuItems = [
    {
@@ -29,18 +30,30 @@ const menuItems = [
 ]
 
 const menuClasses = `absolute top-14 left-16 z-10 divide-y divide-[#3e3e3e]
-bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default hidden group-hover:block text-nowrap`
+bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default text-nowrap`
 
 const Playlist = ({ title, descr, img, classes }) => {
+
+   const [isContextMenuOpen, setIsContextMenuOpen] = useState(false)
+
+   const openContextMenu = (e) => {
+      e.preventDefault()
+
+      setIsContextMenuOpen(true)
+   }
+
    return (
-      <a href="#" className={`relative p-4 rounded-md hover:bg-[#191919] duration-200 group ${classes}`}>
+      <a
+         href="#"
+         className={`relative p-4 rounded-md hover:bg-[#191919] duration-200 group ${classes}`}
+         onContextMenu={openContextMenu} >
          <div className="mb-4 relative">
             <PlaylistImg img={img} />
             <PlaylistBtnPlay />
          </div>
          <PlaylistTitle title={title} />
          <PlaylistDescr descr={descr} />
-         <PlaylistContextMenu menuItems={menuItems} classes={menuClasses} />
+         {isContextMenuOpen && <PlaylistContextMenu menuItems={menuItems} classes={menuClasses} />}
       </a>
    )
 }
