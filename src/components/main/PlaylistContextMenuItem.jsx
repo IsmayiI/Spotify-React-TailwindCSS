8f1aec3671397ed830a772ba6fa6c74ab21b1d1c
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-05-15 14:37:23 +0400

    added a delay

--- src/components/main/PlaylistContextMenuItem.jsx
+++ src/components/main/PlaylistContextMenuItem.jsx
@@ -1,6 +1,6 @@
 import { ChevronRightIcon } from '@heroicons/react/24/outline'
 import PlaylistContextMenu from './PlaylistContextMenu'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const PlaylistContextMenuItem = ({ label, subMenuItems }) => {
    const [menuState, setMenuState] = useState({
@@ -8,6 +8,7 @@ const PlaylistContextMenuItem = ({ label, subMenuItems }) => {
       positionClasses: ''
    })
    const menuItemRef = useRef(null)
+   let closeMenuTimer = null
 
    const getMenuPositionClass = () => {
       const menuItem = menuItemRef.current
@@ -32,6 +33,11 @@ const PlaylistContextMenuItem = ({ label, subMenuItems }) => {
    }
 
    const openMenu = () => {
+      if(closeMenuTimer) {
+         stopCloseMenuTimer()
+
+         return
+      }
       const {x, y} = getMenuPositionClass()
       setMenuState({
          isOpen: true,
@@ -46,11 +52,22 @@ const PlaylistContextMenuItem = ({ label, subMenuItems }) => {
       })
    }
 
+   const startCloseMenuTimer = () => {
+      closeMenuTimer = setTimeout(closeMenu, 1000)
+   }
+
+   const stopCloseMenuTimer = () => {
+      clearTimeout(closeMenuTimer)
+   }
+
+   useEffect(() => stopCloseMenuTimer)
+
+
    let btnClasses = 'w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default text-nowrap'
 
    if (subMenuItems) {
       return (
-         <li ref={menuItemRef} className='relative' onMouseEnter={openMenu} onMouseLeave={closeMenu}>
+         <li ref={menuItemRef} className='relative' onMouseEnter={openMenu} onMouseLeave={startCloseMenuTimer}>
             <button className={`${btnClasses} flex justify-between items-center`}>
                {label}
                <ChevronRightIcon className="h-4 w-4" />
